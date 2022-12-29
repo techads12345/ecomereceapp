4afaff483217e831880b894d05124b24a227070b
techads12345 <techads12345@gmail.com>
2022-12-29 09:13:52 +0530

    add db configuration

---
+++ src/main/java/com/ecomereceapp/EcomereceappApplication.java
@@ -0,0 +1,13 @@
+package com.ecomereceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomereceappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomereceappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecomereceapp/EcomereceappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomereceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomereceappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
