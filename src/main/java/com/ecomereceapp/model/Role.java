e533023e55b8f7a287c5bbcfc1109bcdf5c6192b
techads12345 <techads12345@gmail.com>
2022-12-29 09:22:56 +0530

    added role entity

---
+++ src/main/java/com/ecomereceapp/model/Role.java
@@ -0,0 +1,29 @@
+package com.ecomereceapp.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
